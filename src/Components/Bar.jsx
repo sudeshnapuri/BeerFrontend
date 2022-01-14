4717a70717ff4f93d3d3483f87717c5f37dd67e4
JennyE7 <jenniferjayne.me@hotmail.com>
2022-01-14 15:10:07 +0000

    added create that doesn't work

--- src/Components/Bar.jsx
+++ src/Components/Bar.jsx
@@ -9,6 +9,9 @@ const Bar = () => {
     const [beerData, setBeerData] = useState([]);
     const [error, setError] = useState(null);
     const [tempId, setTempId] = useState(null);
+    const [tempName, setTempName] = useState(null);
+    const [tempDesc, setTempDesc] = useState(null);
+    const [tempAbv, setTempAbv] = useState(null);
 
     const getData = () => {
         console.log("button clicked");
@@ -29,13 +32,33 @@ const Bar = () => {
 
         axios.get(`http://localhost:5015/beer/get/${id}`)
         .then((response) => {
-            setBeerData(response.data);
+            setBeerData([response.data]);
             setLoaded(true);
         }).catch((error) => {
             setLoaded(true);
             setError(error);
         });
+    };
+
+    const deleteBeer = (id) => {
+        axios.delete(`http://localhost:5015/beer/delete/${id}`)
+        .then((res) => {
+            getData();
+            alert(res.data);
+        }).catch((error) => {
+            setError(error);
+        });
     }
+
+    // const createBeer = (beerName, beerDesc, beerAbv) => {
+    //     axios.post(`http://localhost:5015/beer/post`, {
+    //             name: beerName,
+    //             description : beerDesc,
+    //             abv: beerAbv
+    //         }).then((response) => {
+    //             console.log(response);
+    //         }); 
+    // }
 
 
 
@@ -48,6 +71,18 @@ const Bar = () => {
                 <Button onClick={() => {getById(tempId)}}>
                     Get Beer by ID
                 </Button>
+                <Button onClick={() => {deleteBeer(tempId)}}>
+                   Delete Beer by ID
+                </Button>
+
+                {/* <form>
+                    <input type="text" placeholder="name" onChange={(e) => {setTempName(e.target.value)}}/>
+                    <input type="text" placeholder="description" onChange={(e) => {setTempDesc(e.target.value)}}/>
+                    <input type="text" placeholder="ABV" onChange={(e) => {setTempAbv(e.target.value)}}/>
+                    <Button onClick={() => {createBeer(tempName, tempDesc, tempAbv)}}>
+                   Add Beer
+                    </Button>
+                </form> */}
                
                 {beerData.map((beer) => {
                     return <Beer name={beer.name} image={beer.image_url} abv={beer.abv} desc={beer.description}/>
